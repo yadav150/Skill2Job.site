9be2bd0516281b15bef4cc27870b6de9a19ccec5
yadav150 <yadavsubba2003@gmail.com>
2025-10-03 16:09:38 +0530

    Create ga.js

---
+++ ga.js
@@ -0,0 +1,11 @@
+(function() {
+  var gaScript = document.createElement("script");
+  gaScript.async = true;
+  gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-4506817143";
+  document.head.appendChild(gaScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-4506817143');
+})();
