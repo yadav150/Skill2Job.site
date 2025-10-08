4e00931ea029dc0d4f8dd723cd5e02df9d3b9a7a
Yadav Subba <yadavsubba2003@gmail.com>
2025-10-08 06:36:18 +0530

    Update and rename post-date.js to posts-data.json

--- post-date.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const postDateEl = document.getElementById('post-date');
-  if (postDateEl) {
-    const options = { day: 'numeric', month: 'long', year: 'numeric' };
-    postDateEl.textContent = new Date().toLocaleDateString('en-IN', options);
-  }
-});
